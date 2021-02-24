e41a323e2ac02eb49292896515fef26e0e76e006
dhruvpatelgeek <dhruvpatel1999@outlook.in>
2021-02-24 14:29:02 -0800

    FASTER

--- killer/src/client_test.sh
+++ killer/src/client_test.sh
@@ -5,12 +5,12 @@ printf "[KILLER STARTED]*********************\n"
 
 ## for some reason it doesn't kill 4001
 for i in {1..2} ; do
-    go run main.go 127.0.0.1 4001 4 kill_everything
+    go run main.go 127.0.0.1 4001 4 kill_everything &
 done
 
 for i in {1..5} ; do
     for j in {4000..4010} ; do
-        go run main.go 127.0.0.1 $j 4 kill_everything
+        go run main.go 127.0.0.1 $j 4 kill_everything &
     done
 done
 printf "\n[KILLER EXIT]-----------------------\n"
