14eef81f2dcf10a6b3d2fedc8b21db2af509ccf7
dhruvpatelgeek <dhruvpatel1999@outlook.in>
2021-02-24 13:11:24 -0800

    done

--- pa2-dhruvpatelgeek/src/client_test.sh
+++ pa2-dhruvpatelgeek/src/client_test.sh
@@ -1,5 +1,8 @@
 printf "BUILD STARTED @"
 date
 printf "[TEST STARTED]*********************\n"
-go run main.go 127.0.0.1 4001 4 kill_everything
+for i in {4000..4004} ; do
+    go run main.go 127.0.0.1 $i 4 kill_everything
+    go run main.go 127.0.0.1 $i 4 kill_everything
+done
 printf "\n[TEST EXIT]-----------------------\n"
