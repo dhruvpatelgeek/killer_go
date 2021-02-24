a10d404858f2ed4081337bbb2a5e70605b3f951b
dhruvpatelgeek <dhruvpatel1999@outlook.in>
2021-02-24 13:16:42 -0800

    killer done

--- pa2-dhruvpatelgeek/src/client_test.sh
+++ pa2-dhruvpatelgeek/src/client_test.sh
@@ -1,8 +1,13 @@
 printf "BUILD STARTED @"
 date
-printf "[TEST STARTED]*********************\n"
+printf "[KILLER STARTED]*********************\n"
 for i in {4000..4004} ; do
     go run main.go 127.0.0.1 $i 4 kill_everything
-    go run main.go 127.0.0.1 $i 4 kill_everything
+       go run main.go 127.0.0.1 $i 4 kill_everything
+          go run main.go 127.0.0.1 $i 4 kill_everything
+             go run main.go 127.0.0.1 $i 4 kill_everything
 done
-printf "\n[TEST EXIT]-----------------------\n"
+
+## for some reason it doesn't kill 4001
+ go run main.go 127.0.0.1 4001 4 kill_everything
+printf "\n[KILLER EXIT]-----------------------\n"
